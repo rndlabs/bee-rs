3580944cafe885e180ae6ff66c2fc95e38819194
mfw78 <mfw78@protonmail.com>
2023-07-16 23:13:08 +0000

    feat(wasm): tracing

--- bin/wasm-playground/src/lib.rs
+++ bin/wasm-playground/src/lib.rs
@@ -1,10 +1,14 @@
+use tracing::{debug, error, info, trace, warn};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::window;
-use web_sys::{Event, HtmlDivElement, HtmlInputElement, HtmlButtonElement};
+use web_sys::{Event, HtmlButtonElement, HtmlDivElement, HtmlInputElement};
 
 #[wasm_bindgen(start)]
 fn main() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+    tracing_wasm::set_as_global_default();
+
     let document = web_sys::window().unwrap().document().unwrap();
 
     // Create a new div element to contain the input and button
@@ -27,11 +31,20 @@ fn main() -> Result<(), JsValue> {
     button.set_inner_text("Calculate");
     container_div.append_child(&button).unwrap();
 
+    info!("Created input and button elements");
     let closure = Closure::wrap(Box::new(move |_event: Event| {
         // Build a chunk with the input text. The chunk size is 4096 bytes.
         let f = bmt::file::ChunkedFile::new(input.value().as_bytes().to_vec(), bmt::chunk::Options { max_payload_size: 4096 });
         // Display the swarm hash
-        window().unwrap().alert_with_message(&format!("Swarm hash: {:#?}", hex::encode(f.address()))).unwrap();
+        window().unwrap().alert_with_message(&format!("Swarm hash: {:?}", hex::encode(f.address()))).unwrap();
+
+        // Display the leaf chunks with their contents and hashes in an alert window
+        let chunks = f.leaf_chunks();
+        let mut chunks_str = String::new();
+        for chunk in chunks {
+            chunks_str.push_str(&format!("Chunk: {:?}\n", chunk));
+        }
+        window().unwrap().alert_with_message(&chunks_str).unwrap();
     }) as Box<dyn FnMut(Event)>);
 
     button.set_onclick(Some(closure.as_ref().unchecked_ref()));
