af955d24dc1c1d9716a02ecab864d318b1fa583a
mfw78 <mfw78@protonmail.com>
2023-07-16 23:28:22 +0000

    feat(postage): initial commit

---
+++ crates/postage/src/stamp.rs
@@ -0,0 +1,268 @@
+use std::{pin::Pin, future::Future};
+
+use crate::{batch::BatchId, pat::BucketSeeker};
+use ethers_core::{abi::Address, types::{Signature, H256}};
+use thiserror::Error;
+use tiny_keccak::{Hasher, Keccak};
+use tracing::{debug, error, info, trace, warn};
+
+use bmt::chunk::Chunk;
+
+// Define a type alias for a closure that takes a chunk + stamp, and modifies the chunk
+// to include the stamp if it is valid
+pub type ValidateStamp<'a> =
+    Box<dyn FnMut(&mut Chunk, MarshalledStamp) -> Result<(), StampError> + 'a>;
+
+pub type MarshalledStamp = [u8; 113];
+
+pub trait StampValidator {
+    fn validate_stamp<'a>(&'a self) -> ValidateStamp<'a>;
+}
+
+/// An error involving a stamp
+#[derive(Debug, Error)]
+pub enum StampError {
+    /// Invalid signature
+    #[error("owner mismatch, expected {0}, got {1}")]
+    OwnerMismatch(Address, Address),
+    /// Invalid index
+    /// The index is equal to a two `u32` concatenated together where the first `u32` is the
+    /// bucket and the second `u32` is the index in the bucket
+    #[error("invalid index")]
+    InvalidIndex(),
+    /// The chunk address does not match the bucket
+    #[error("bucket mismatch")]
+    BucketMismatch(),
+    /// When a batch isn't found in the store
+    #[error("batch not found")]
+    BatchNotFound(BatchId),
+}
+
+/// A `Stamp` represents the proof of postage for a chunk.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Stamp {
+    batch: BatchId, // the batch id
+    x: u32,         // which collision bucket the chunk is in
+    y: u32,         // where in bucket the chunk is
+    timestamp: u64, // timestamp of the stamp
+    sig: [u8; 65],  // signature of the stamp
+}
+
+// Define a type alias for a closure that takes a digest and returns a signature
+impl Stamp {
+    pub async fn new<'a, F>(
+        chunk: &Chunk,
+        batch: BatchId,
+        x: u32,
+        y: u32,
+        timestamp: u64,
+        sig_fn: F,
+    ) -> Self
+    where
+        F: 'a + Fn([u8; 32]) -> Pin<Box<dyn Future<Output = Result<[u8; 65], Box<dyn std::error::Error + 'a>>>>>,
+    {
+        Self {
+            batch,
+            x,
+            y,
+            timestamp,
+            sig: (sig_fn)(Self::digest(&chunk, batch, x, y, timestamp)).await.unwrap(),
+        }
+    }
+
+    /// Returns the hash of the stamp to be signed
+    /// This is equal to H(chunkAddr || batchId || sillyIndex || timestamp)
+    pub fn digest(chunk: &Chunk, batch: BatchId, x: u32, y: u32, timestamp: u64) -> BatchId {
+        let mut hasher = Keccak::v256();
+        hasher.update(&chunk.address().as_slice());
+        hasher.update(&batch);
+        hasher.update(&Self::silly_index(x, y).to_be_bytes());
+        hasher.update(&timestamp.to_be_bytes());
+        let mut digest = [0u8; 32];
+        hasher.finalize(&mut digest);
+        digest
+    }
+
+    /// This is equal to a two `u32` concatenated
+    /// The first `u32` is the bucket (`x`)
+    /// The second `u32` is the bucket index (`y`)
+    fn silly_index(x: u32, y: u32) -> u64 {
+        let mut bytes = [0u8; 8];
+        bytes[..4].copy_from_slice(&x.to_be_bytes());
+        bytes[4..].copy_from_slice(&y.to_be_bytes());
+        u64::from_be_bytes(bytes)
+    }
+
+    pub fn valid(&self, chunk: &Chunk, owner: Address, x: u32, y: u32) -> Result<bool, StampError> {
+        // check `x` - we are in the correct bucket
+        if self.x != chunk.get_x(y) {
+            return Err(StampError::BucketMismatch());
+        }
+
+        // check `y` does not exceed the max number of chunks in a bucket
+        // this is equal to 2^(depth - bucket_depth) and is zero indexed
+        if self.y >= 1 << (x - y) {
+            return Err(StampError::InvalidIndex());
+        }
+
+        let digest = Self::digest(chunk, self.batch, self.x, self.y, self.timestamp);
+
+        // verify the signature
+        // using unwrap() here is safe because we know the signature is 65 bytes
+        Signature::try_from(self.sig.as_slice())
+            .unwrap()
+            .recover(digest)
+            .map_err(|_| StampError::InvalidIndex())
+            .and_then(|recovered| {
+                if owner == recovered {
+                    Ok(true)
+                } else {
+                    Err(StampError::OwnerMismatch(owner, recovered))
+                }
+            })
+    }
+
+    pub fn batch(&self) -> [u8; 32] {
+        self.batch
+    }
+}
+
+impl From<Stamp> for MarshalledStamp {
+    fn from(stamp: Stamp) -> Self {
+        let mut bytes = [0u8; 113];
+        bytes[..32].copy_from_slice(&stamp.batch);
+        bytes[32..36].copy_from_slice(&stamp.x.to_be_bytes());
+        bytes[36..40].copy_from_slice(&stamp.y.to_be_bytes());
+        bytes[40..48].copy_from_slice(&stamp.timestamp.to_be_bytes());
+        bytes[48..113].copy_from_slice(&stamp.sig.to_vec().as_slice());
+        bytes
+    }
+}
+
+impl From<MarshalledStamp> for Stamp {
+    fn from(bytes: MarshalledStamp) -> Self {
+        let mut batch = [0u8; 32];
+        batch.copy_from_slice(&bytes[..32]);
+        let x = u32::from_be_bytes(bytes[32..36].try_into().unwrap());
+        let y = u32::from_be_bytes(bytes[36..40].try_into().unwrap());
+        let timestamp = u64::from_be_bytes(bytes[40..48].try_into().unwrap());
+        let mut sig = [0u8; 65];
+        sig.copy_from_slice(&bytes[48..113]);
+        Self {
+            batch,
+            x,
+            y,
+            timestamp,
+            sig,
+        }
+    }
+}
+
+impl From<Stamp> for Vec<u8> {
+    fn from(stamp: Stamp) -> Self {
+        let mut bytes = Vec::new();
+        bytes.extend_from_slice(&stamp.batch);
+        bytes.extend_from_slice(&stamp.x.to_be_bytes());
+        bytes.extend_from_slice(&stamp.y.to_be_bytes());
+        bytes.extend_from_slice(&stamp.timestamp.to_be_bytes());
+        bytes.extend_from_slice(&stamp.sig.to_vec().as_slice());
+        bytes
+    }
+}
+
+impl From<Vec<u8>> for Stamp {
+    fn from(bytes: Vec<u8>) -> Self {
+        let mut batch = [0u8; 32];
+        batch.copy_from_slice(&bytes[..32]);
+        let x = u32::from_be_bytes(bytes[32..36].try_into().unwrap());
+        let y = u32::from_be_bytes(bytes[36..40].try_into().unwrap());
+        let timestamp = u64::from_be_bytes(bytes[40..48].try_into().unwrap());
+        let mut sig = [0u8; 65];
+        sig.copy_from_slice(&bytes[48..113]);
+        Self {
+            batch,
+            x,
+            y,
+            timestamp,
+            sig: sig.try_into().unwrap(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lazy_static::lazy_static;
+
+    lazy_static! {
+        static ref BATCH_ID: Vec<u8> = hex::decode("c3387832bb1b88acbcd0ffdb65a08ef077d98c08d4bee576a72dbe3d36761369").unwrap();
+        static ref STAMP_MARSHALLED: Vec<u8> = hex::decode("c3387832bb1b88acbcd0ffdb65a08ef077d98c08d4bee576a72dbe3d367613690000cbe5000000000000018921ff0dbb29169df9e6364e26c6ca6b17745c10b9d6a36ea38e204f2e3cc64a8373c0661f5bb0a347c61d8d1689b0dcf8354117686a6a18d08cff927f526de5fc61b2b7491b").unwrap();
+        static ref ADDRESS: Vec<u8> = hex::decode("cbe563e4865fd01948a1180081bbb7e144204344012dea8ce6e86d36dbc63495").unwrap();
+        static ref PAYLOAD: Vec<u8> = hex::decode("0b0000000000000068656c6c6f20776f72646c").unwrap();
+        static ref BUCKET_INDEX: u32 = 0;
+        static ref BUCKET: u32 = 52197;
+        static ref TIMESTAMP: u64 = 1688492510651;
+    }
+
+    // const expectedChunkedStamped_0 = [
+    //     {
+    //       address: "cbe563e4865fd01948a1180081bbb7e144204344012dea8ce6e86d36dbc63495",
+    //       payload: Buffer.from("0b0000000000000068656c6c6f20776f72646c", "hex"),
+    //       stamp: Buffer.from("c3387832bb1b88acbcd0ffdb65a08ef077d98c08d4bee576a72dbe3d367613690000cbe5000000000000018921ff0dbb29169df9e6364e26c6ca6b17745c10b9d6a36ea38e204f2e3cc64a8373c0661f5bb0a347c61d8d1689b0dcf8354117686a6a18d08cff927f526de5fc61b2b7491b", "hex")
+    //     }
+    //   ]
+
+    // test('chunkerStampers', async () => {
+    //     const timeStamp = 1688492510651;
+    //     const payload = Buffer.from("hello wordl")
+    //     const batchID = 'c3387832bb1b88acbcd0ffdb65a08ef077d98c08d4bee576a72dbe3d36761369' // process.argv[3]
+    //     const privateKey = 'be52c649a4c560a1012daa572d4e81627bcce20ca14e007aef87808a7fadd3d0' // process.argv[4]
+    //     const chunkedStamped = await chunkerStamper(payload, batchID, privateKey, timeStamp)
+
+    //     expect(chunkedStamped.length).toBe(1)
+    //     expect(chunkedStamped[0].address).toEqual(expectedChunkedStamped_0[0].address)
+    //     expect(chunkedStamped[0].stamp).toEqual(expectedChunkedStamped_0[0].stamp)
+    //     expect(chunkedStamped[0].payload).toEqual(expectedChunkedStamped_0[0].payload)
+    // })
+
+    #[test]
+    fn stamp_to_bytes() {
+        let stamp = Stamp {
+            batch: [0u8; 32],
+            y: 0,
+            x: 0,
+            timestamp: 0,
+            sig: [0u8; 65],
+        };
+        let bytes: MarshalledStamp = stamp.into();
+        assert_eq!(bytes.len(), 113);
+    }
+
+    #[test]
+    fn stamp_from_bytes() {
+        let stamp: Stamp = STAMP_MARSHALLED.clone().into();
+        assert_eq!(&stamp.batch[..], &BATCH_ID[..]);
+        assert_eq!(stamp.x, *BUCKET);
+        assert_eq!(stamp.y, *BUCKET_INDEX);
+        assert_eq!(stamp.timestamp, *TIMESTAMP);
+        assert_eq!(stamp.sig, STAMP_MARSHALLED[48..113]);
+    }
+
+    #[test]
+    fn stamp_to_vec() {
+        let stamp: Stamp = STAMP_MARSHALLED.clone().into();
+        let vec: Vec<u8> = stamp.clone().into();
+        let bytes: MarshalledStamp = stamp.into();
+
+        assert_eq!(vec, bytes.to_vec());
+    }
+
+    #[test]
+    fn stamp_from_vec() {
+        let stamp: Stamp = STAMP_MARSHALLED.clone().into();
+        let vec: Vec<u8> = stamp.clone().into();
+        let stamp_from_vec: Stamp = vec.into();
+
+        assert_eq!(stamp, stamp_from_vec);
+    }
+}
