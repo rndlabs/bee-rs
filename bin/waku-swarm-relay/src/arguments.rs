61eb9e11e08a14960da54aaf0c869875600217d6
mfw78 <mfw78@protonmail.com>
2023-07-29 08:12:23 +0000

    chore: arguments

--- bin/waku-swarm-relay/src/arguments.rs
+++ bin/waku-swarm-relay/src/arguments.rs
@@ -1,3 +1,5 @@
+use waku_bindings::Multiaddr;
+
 #[derive(clap::Parser)]
 #[group(skip)]
 pub struct Arguments {
@@ -5,6 +7,10 @@ pub struct Arguments {
     pub logging: logging::LoggingArguments,
     #[clap(long, help = "Private key for relay operations", value_parser = parse_private_key)]
     pub private_key: String,
+    #[clap(long, help = "Peer id of the relay to connect to", value_parser = parse_multiaddr)]
+    pub peer: Vec<Multiaddr>,
+    #[clap(long, help = "enrtree")]
+    pub enrtree: Option<String>,
 }
 
 // Write a parser to make sure that the private_key is a valid ethereum private key
@@ -18,4 +24,10 @@ pub fn parse_private_key(private_key: &str) -> Result<String, String> {
         return Err(String::from("Invalid private key"));
     }
     Ok(String::from(private_key))
+}
+
+pub fn parse_multiaddr(multiaddr: &str) -> Result<Multiaddr, String> {
+    multiaddr
+        .parse()
+        .map_err(|_| String::from("Invalid multiaddr"))
 }
