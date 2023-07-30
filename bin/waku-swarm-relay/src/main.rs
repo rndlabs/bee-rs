8b15035a6618baaff84f12b4294d98abad5a18c4
mfw78 <mfw78@protonmail.com>
2023-07-30 15:58:04 +0000

    feat(waku-swarm-relay): retrieval protocol

--- bin/waku-swarm-relay/src/main.rs
+++ bin/waku-swarm-relay/src/main.rs
@@ -4,6 +4,7 @@ mod protocol;
 use arguments::Arguments;
 use clap::Parser;
 use ethers_signers::{LocalWallet, Signer};
+use reqwest::Client;
 use tracing::{debug, error, info};
 use waku_bindings::{
     waku_new, waku_set_event_callback, Encoding, Running, WakuContentTopic, WakuLogLevel,
@@ -16,7 +17,7 @@ use std::time::Duration;
 use chrono::Utc;
 use prost::Message;
 
-use crate::protocol::{Ping, Pong};
+use crate::protocol::{Ping, Pong, RetrievalDelivery, RetrievalRequest};
 
 struct App {
     pub node_handle: WakuNodeHandle<Running>,
@@ -62,12 +63,6 @@ pub fn setup_node_handle(args: &Arguments) -> Result<WakuNodeHandle<Running>, Wa
     Ok(node_handle)
 }
 
-#[derive(Debug, Clone)]
-pub struct ReceivedMessage {
-    pub payload: Vec<u8>,
-    pub content_topic: WakuContentTopic,
-}
-
 #[tokio::main]
 async fn main() {
     let args = crate::arguments::Arguments::parse();
@@ -92,6 +87,10 @@ async fn main() {
 
     let _ping_topic: WakuContentTopic = "/swarm-waku/1/ping/proto".parse().unwrap();
     let pong_topic: WakuContentTopic = "/swarm-waku/1/pong/proto".parse().unwrap();
+    let retrieval_request_topic: WakuContentTopic =
+        "/swarm-waku/1/retrieval-request/proto".parse().unwrap();
+    let retrieval_delivery_topic: WakuContentTopic =
+        "/swarm-waku/1/retrieval-delivery/proto".parse().unwrap();
 
     let pubsub_topic = WakuPubSubTopic::new(PUBSUB, Encoding::Proto);
 
@@ -168,9 +167,79 @@ async fn main() {
                     }
                 }
             }
+            "retrieval-request" => {
+                debug!(
+                    retrieval_request = tracing::field::debug(msg.payload().clone()),
+                    "Received retrieval request"
+                );
+
+                let request = match RetrievalRequest::decode(msg.payload()) {
+                    Ok(request) => request,
+                    Err(e) => {
+                        error!("Failed to decode retrieval request: {}", e);
+                        continue;
+                    }
+                };
+
+                // At this point we have the chunk address which to retrieve
+
+                // Make an asynchronous HTTP request to the remote API URI
+                let api_uri = args.bee_api_url.to_owned()
+                    + "/chunks/"
+                    + &hex::encode(request.chunk_address.as_slice());
+                debug!("Fetching data from remote API: {}", api_uri);
+                let response = match fetch_from_remote_api(&api_uri).await {
+                    Ok(response) => response,
+                    Err(e) => {
+                        error!("Failed to fetch data from remote API: {}", e);
+                        // Handle the error and continue or return an error response to the user
+                        continue;
+                    }
+                };
+
+                // Send the API response to the user
+                // craft the retrieval delivery
+                let delivery = RetrievalDelivery {
+                    data: response,
+                    stamp: vec![],
+                };
+                let mut buf = Vec::new();
+                match delivery.encode(&mut buf) {
+                    Ok(_) => {
+                        // create the WakuMessage
+                        let waku_message = WakuMessage::new(
+                            buf,
+                            retrieval_delivery_topic.clone(),
+                            1,
+                            Utc::now().timestamp_millis() as usize,
+                            vec![],
+                            true,
+                        );
+                        let msg_id = app
+                            .node_handle
+                            .relay_publish_message(&waku_message, Some(pubsub_topic.clone()), None)
+                            .unwrap();
+                        debug!(
+                            "Published retrieval response: {:?} msg_id: {:?}",
+                            waku_message, msg_id
+                        );
+                    }
+                    Err(e) => {
+                        error!("Failed to encode retrieval response: {}", e);
+                        continue;
+                    }
+                }
+            }
             _ => {}
         }
     }
+}
+
+// Asynchronous function to perform the HTTP request using reqwest
+async fn fetch_from_remote_api(api_uri: &str) -> Result<Vec<u8>, reqwest::Error> {
+    let client = Client::new();
+    let response = client.get(api_uri).send().await?.bytes().await?.to_vec();
+    Ok(response)
 }
 
 #[derive(Debug, thiserror::Error)]
